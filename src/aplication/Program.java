6c007bbee30da610122dfbae2585150b3d487dd0
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-06 10:55:15 -0300

    Primeira Classe: Posicao

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,10 +1,12 @@
 package aplication;
 
+import tabuleiro.Posicao;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("ola mundo");
-
+		Posicao pos = new Posicao (3 , 5);
+		System.out.println(pos);
 	}
 
 }
