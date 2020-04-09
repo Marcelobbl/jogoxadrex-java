ae52e02fd2926e850605ae65f2c922396ba069ff
Marcelobbl <produtormarcelo@hotmail.com>
2020-04-09 11:50:00 -0300

    Implementando movimentos possiveis do rei

--- src/xadrex/pecas/Rei.java
+++ src/xadrex/pecas/Rei.java
@@ -1,6 +1,7 @@
 package xadrex.pecas;
 
 import tabuleiro.Borda;
+import tabuleiro.Posicao;
 import xadrex.Cor;
 import xadrex.PecaXadrex;
 
@@ -13,9 +14,50 @@ public class Rei extends PecaXadrex {
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean possoMover(Posicao posicao) {
+		PecaXadrex p = (PecaXadrex)getBorda().peca(posicao);
+		return p == null || p.getCor() != getCor();
+		}
 	@Override
 	public boolean[][] possivelMovimentos() {
 		boolean[][] mat = new boolean [getBorda().getLinhas()][getBorda().getColunas()];
+		
+		Posicao p = new Posicao (0,0);
+		p.setValores(posicao.getLinha() - 1 , posicao.getColuna());
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		p.setValores(posicao.getLinha() - 1 , posicao.getColuna() + 1);
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		p.setValores(posicao.getLinha()  - 1 , posicao.getColuna() - 1);
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		
+		p.setValores(posicao.getLinha()  , posicao.getColuna() + 1);
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		p.setValores(posicao.getLinha()   , posicao.getColuna() - 1);
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		p.setValores(posicao.getLinha() + 1 , posicao.getColuna());
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		p.setValores(posicao.getLinha() + 1 , posicao.getColuna() + 1);
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+		p.setValores(posicao.getLinha()  + 1 , posicao.getColuna() - 1);
+		if (getBorda().posicaoExiste(p) && possoMover(p)) {
+			mat[p.getLinha()][p.getColuna()] = true;
+		}
+			
 		return mat;
 	}
 
